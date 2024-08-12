e4b0f711e0faebe31f942b0a45f0f3b49cf25c80
Arman Hossain Jabed <87227303+jabed11@users.noreply.github.com>
2024-08-13 00:21:35 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log("Hello");
+// console.log("I like custard");
+
+// window.alert(`This is an alert`);
+// window.alert(`I like custard`);
+
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myP").textContent = `I like custard`;
+
+// let age = 23;
+// let price = 52.66;
+// console.log('You are '+(age)+' years old');
+// console.log('The price is $'+(price));
+
+let firstName = "Arman";
+
+console.log(firstName);
+console.log(`You are ${firstName}`);
+//name
